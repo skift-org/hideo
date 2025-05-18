f8e944e8cf2156ae4ea575813ace1b2e4f78520c
sleepy-monax <me@smnx.sh>
2025-05-18 11:12:27 +0200

    hideo-about: Update copyright.

--- src/hideo-about/app.cpp
+++ src/hideo-about/app.cpp
@@ -25,7 +25,6 @@ export Ui::Child app() {
                 auto bodyText =
                     Ui::bodySmall(
                         "Copyright © 2018-2024 The skiftOS Developers\n"
-                        "Copyright © 2024 Odoo S.A.\n"
                         "\n"
                         "All rights reserved."
                     );

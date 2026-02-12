b95d33a4600fe01301b53d4689d1b00a0b5298b8
sleepy-monax <me@smnx.sh>
2026-02-12 22:03:46 +0100

    hideo-canvas: Initial tool implementation.

--- src/hideo-canvas/app.cpp
+++ src/hideo-canvas/app.cpp
@@ -7,31 +7,203 @@ import Karm.Kira;
 import Karm.Gfx;
 import Karm.Math;
 import Karm.Kira;
+import Karm.App;
 
 using namespace Karm;
 
 namespace Hideo::Canvas {
 
-Ui::Child canvas() {
-    return Ui::vflow(
-               2,
-               Ui::labelSmall(Gfx::GRAY400, "Frame 1"),
-               Ui::empty({320, 240}) |
-                   Ui::box({
-                       .backgroundFill = Gfx::WHITE,
-                   })
-           ) |
-           Ui::center() | Ui::bound() | Kr::selectionArea();
+enum struct Tool {
+    SELECT,
+    FRAME,
+    TEXT,
+    RECT,
+};
+
+enum struct Mode {
+    IDLE,
+    PLACING,
+    RESIZING,
+    SELECTING
+};
+
+struct Object {
+    u64 id;
+    Math::Rectf bound;
+
+    Gfx::Color backgroundColor;
+};
+
+struct State {
+    u64 idAllocator = 1;
+    Tool currentTool = Tool::SELECT;
+    Mode currentMode = Mode::IDLE;
+    Vec<u64> selected;
+    Math::Vec2f dragStart;
+    Math::Vec2f dragEnd;
+    Vec<Object> objects;
+
+    Opt<u64> objectAt(Math::Vec2f pos) {
+        for (auto& o : iterRev(objects)) {
+            if (o.bound.contains(pos))
+                return o.id;
+        }
+        return NONE;
+    }
+
+    Vec<u64> objectAt(Math::Rectf rect) {
+        Vec<u64> res;
+        for (auto& o : objects) {
+            if (o.bound.colide(rect))
+                res.pushBack(o.id);
+        }
+        return res;
+    }
+};
+
+struct SelectTool {
+    Tool tool;
+};
+
+struct CanvasPress {
+    Math::Vec2f pos;
+};
+
+struct CanvasRelease {
+    Math::Vec2f pos;
+};
+
+struct CanvasDrag {
+    Math::Vec2f pos;
+};
+
+using Action = Union<SelectTool, CanvasPress, CanvasRelease, CanvasDrag>;
+
+Ui::Task<Action> reduce(State& s, Action a) {
+    a.visit(Visitor{
+        [&](SelectTool a) {
+            s.currentTool = a.tool;
+        },
+        [&](CanvasPress a) {
+            s.dragStart = a.pos;
+            s.dragEnd = a.pos;
+            if (s.currentTool == Tool::SELECT) {
+                s.selected.clear();
+                if (auto id = s.objectAt(a.pos); id.has()) {
+                    s.selected = {id.unwrap()};
+                } else {
+                    s.currentMode = Mode::SELECTING;
+                }
+            } else {
+                s.objects.pushBack({
+                    .id = s.idAllocator++,
+                    .bound = {
+                        a.pos,
+                        {},
+                    },
+                    .backgroundColor = s.currentTool == Tool::FRAME ? Gfx::WHITE : Gfx::GRAY400,
+                });
+                s.currentMode = Mode::PLACING;
+            }
+        },
+        [&](CanvasRelease d) {
+            if (s.currentMode == Mode::PLACING) {
+                if (s.dragStart.dist(s.dragEnd) < 2) {
+                    last(s.objects).bound = Math::Rectf::fromTwoPoint(d.pos, d.pos).grow(50);
+                }
+            }
+
+            s.currentMode = Mode::IDLE;
+            s.currentTool = Tool::SELECT;
+        },
+        [&](CanvasDrag d) {
+            s.dragEnd = d.pos;
+            if (s.currentMode == Mode::RESIZING or s.currentMode == Mode::PLACING) {
+                last(s.objects).bound = Math::Rectf::fromTwoPoint(s.dragStart, d.pos);
+            } else if (s.currentMode == Mode::SELECTING) {
+                s.selected = s.objectAt(Math::Rectf::fromTwoPoint(s.dragStart, d.pos));
+            }
+        },
+    });
+
+    return NONE;
 }
 
-Ui::Child panelBottom() {
+using Model = Ui::Model<State, Action, reduce>;
+
+// MARK: Model -----------------------------------------------------------------
+
+struct Canvas : Ui::View<Canvas> {
+    State const& _state;
+    Ui::MouseListener _listener;
+
+    Canvas(State const& state)
+        : _state(state) {}
+
+    void paint(Gfx::Canvas& g, Math::Recti) override {
+        g.push();
+        g.clip(bound());
+
+        for (auto& o : _state.objects) {
+            g.push();
+            g.beginPath();
+            g.rect(o.bound);
+            g.fill(o.backgroundColor);
+
+            if (contains(_state.selected, o.id))
+                g.stroke({.fill = Ui::ACCENT500, .width = 1});
+            g.pop();
+        }
+
+        if (_state.currentMode == Mode::SELECTING) {
+            Kr::paintSelection(g, Math::Rectf::fromTwoPoint(_state.dragStart, _state.dragEnd));
+        }
+        g.pop();
+    }
+
+    void event(App::Event& event) override {
+        if (auto e = event.is<App::MouseEvent>(); e and bound().contains(e->pos)) {
+            switch (e->type) {
+            case App::MouseEvent::PRESS:
+                Model::bubble<CanvasPress>(*this, {e->pos.cast<f64>()});
+                break;
+            case App::MouseEvent::RELEASE:
+                Model::bubble<CanvasRelease>(*this, {e->pos.cast<f64>()});
+                break;
+            case App::MouseEvent::SCROLL:
+                break;
+            case App::MouseEvent::MOVE:
+                if (e->buttons.has(App::MouseButton::LEFT))
+                    Model::bubble<CanvasDrag>(*this, {e->pos.cast<f64>()});
+                break;
+            default:
+                unreachable();
+            }
+        }
+    }
+};
+
+Ui::Child canvas(State const& s) {
+    return makeRc<Canvas>(s);
+}
+
+Ui::Child toolbarButton(State const& s, Tool tool, Gfx::Icon icon) {
+    return Ui::button(
+        Model::bind<SelectTool>(tool),
+        s.currentTool == tool
+            ? Ui::ButtonStyle::subtle().withForegroundFill(Ui::ACCENT500)
+            : Ui::ButtonStyle::subtle(),
+        icon
+    );
+}
+
+Ui::Child toolbar(State const& s) {
     return Ui::hflow(
                4,
-               Ui::button(Ui::SINK<>, Ui::ButtonStyle::subtle().withForegroundFill(Ui::ACCENT500), Mdi::CURSOR_DEFAULT),
-               Ui::button(Ui::SINK<>, Ui::ButtonStyle::subtle(), Mdi::ARTBOARD),
-               Ui::button(Ui::SINK<>, Ui::ButtonStyle::subtle(), Mdi::FORMAT_TEXT),
-               Ui::button(Ui::SINK<>, Ui::ButtonStyle::subtle(), Mdi::RECTANGLE_OUTLINE),
-               Ui::button(Ui::SINK<>, Ui::ButtonStyle::subtle(), Mdi::CHEVRON_UP)
+               toolbarButton(s, Tool::SELECT, Mdi::CURSOR_DEFAULT),
+               toolbarButton(s, Tool::FRAME, Mdi::ARTBOARD),
+               toolbarButton(s, Tool::RECT, Mdi::RECTANGLE),
+               toolbarButton(s, Tool::TEXT, Mdi::FORMAT_TEXT)
            ) |
            Ui::box({
                .margin = 16,
@@ -42,7 +214,7 @@ Ui::Child panelBottom() {
            });
 }
 
-Ui::Child panelLeft() {
+Ui::Child documentPanel() {
     return Ui::vflow(
                Kr::labelRow("Layers"s)
            ) |
@@ -52,7 +224,7 @@ Ui::Child panelLeft() {
            Ui::minSize({240, Ui::UNCONSTRAINED});
 }
 
-Ui::Child panelRight() {
+Ui::Child propertiesPanel() {
     return Ui::vflow(
                Kr::labelRow("Postion"s),
                Kr::separator(),
@@ -74,18 +246,20 @@ Ui::Child panelRight() {
 }
 
 export Ui::Child app() {
-    return Kr::scaffold({
-        .icon = Mdi::DRAW,
-        .title = "Canvas"s,
-        .body = [] {
-            return Ui::hflow(
-                panelLeft(),
-                Kr::separator(),
-                Ui::stack(canvas(), panelBottom() | Ui::align(Math::Align::BOTTOM | Math::Align::HCENTER)) | Ui::grow(),
-                Kr::separator(),
-                panelRight()
-            );
-        },
+    return Ui::reducer<Model>([](State const& s) {
+        return Kr::scaffold({
+            .icon = Mdi::DRAW,
+            .title = "Canvas"s,
+            .body = [&] {
+                return Ui::hflow(
+                    documentPanel(),
+                    Kr::separator(),
+                    Ui::stack(canvas(s), toolbar(s) | Ui::align(Math::Align::BOTTOM | Math::Align::HCENTER)) | Ui::grow(),
+                    Kr::separator(),
+                    propertiesPanel()
+                );
+            },
+        });
     });
 }
 
